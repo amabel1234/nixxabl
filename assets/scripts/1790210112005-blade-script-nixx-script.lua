746285eec446f09a55dfa7f1e103d66cc024d45c
amabel1234 <lolipopajakok12@gmail.com>
2026-09-24 07:35:12 +0700

    Add NIXXTEAM script product: BLADE SCRIPT NIXX

---
+++ assets/scripts/1790210112005-blade-script-nixx-script.lua
@@ -0,0 +1,20 @@
+**🚀 NEW UPDATE — NIXXHUB BLADE SPIN**
+
+@everyone
+
+✨ Script:
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/juanzzvy-a11y/vvipnew/refs/heads/main/bladespin'))()
+
+
+🔑 Akses Key:
+https://www.nixcooll.biz.id
+
+📌 Cara Mendapatkan Akses:
+• Daftarkan username kalian di website Nixx
+• Minimal memiliki **1 akun** yang terdaftar
+• Setelah terdaftar, kalian mendapatkan akses selama 1 hari
+• Jika waktu akses sudah habis, kalian bisa daftar ulang untuk mendapatkan key baru ✨
+
+🔥 Selamat mencoba & enjoy! 🤗
